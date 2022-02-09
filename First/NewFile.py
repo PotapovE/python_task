11e53b362df21d56ded9a368ff77f87370dbf1c0
PotapovE <potapovomsk@gmail.com>
2022-02-10 00:41:14 +0300

    task3 rev 1

--- First/NewFile.py
+++ First/NewFile.py
@@ -1,10 +1,6 @@
 # 3. Вывести на экран числа от -N до N
+import math
 
-def show (n):
-    first_array = []
-    if n < 0:
-        n *= -1
-    for i in range(-n, n + 1):
-        first_array.append(i)
-    return first_array
-print(show(int(input())))
+n = abs(int(input()))
+data = [i for i in range(-n, n+1)]
+print (data)
