affe60e23eb4ed0e9e5eef23a197ac483709e988
PotapovE <potapovomsk@gmail.com>
2022-02-11 00:01:01 +0300

    task7

--- First/NewFile.py
+++ First/NewFile.py
@@ -1,1 +1,9 @@
-# 
+# Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
+
+from operator import truediv
+x = True
+y = False
+z = True
+a = (not (x or y or z))
+b = (not x and not y and not z)
+print(a == b)
