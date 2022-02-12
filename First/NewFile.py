388cb05a800a6c4cd68c9ab34ba552402917055a
PotapovE <potapovomsk@gmail.com>
2022-02-12 16:18:41 +0300

    task8

--- First/NewFile.py
+++ First/NewFile.py
@@ -1,1 +1,14 @@
-# 
+# Сообщить в какой четверти координатной плоскости или на какой оси находится точка с координатами Х и У
+
+def answer(x, y):
+    if x > 0 and y > 0 : return '1'
+    elif x < 0 and y > 0 : return '2'
+    elif x < 0 and y < 0 : return '3'
+    elif x > 0 and y < 0 : return '4'
+    if x == 0 and y == 0 : return '1, 2, 3, 4'
+    elif x == 0 and y > 0 : return '1, 2'
+    elif x == 0 and y < 0 : return '3, 4'
+    elif x > 0 and y == 0 : return '1, 4'
+    elif x < 0 and y == 0 : return '2, 3'
+    
+print(answer(-3, 0))
