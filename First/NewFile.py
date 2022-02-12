53dca4f238e876227e1fb812b5b834f3efce6272
PotapovE <potapovomsk@gmail.com>
2022-02-12 16:56:03 +0300

    task10

--- First/NewFile.py
+++ First/NewFile.py
@@ -1,1 +1,8 @@
-# 
+# Найти расстояние между двумя точками пространства
+import math
+
+A = (0, 4)
+B = (0, 6)
+def answer (point_a, point_b):
+    return math.sqrt(math.pow(point_b[0]-point_a[0], 2) + math.pow(point_b[1]-point_a[1], 2))
+print(answer(A, B))
