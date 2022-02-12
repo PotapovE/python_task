e5748e19aa082d49c52b7d82dab75a000cd7d28a
PotapovE <potapovomsk@gmail.com>
2022-02-12 16:31:56 +0300

    task8

--- First/NewFile.py
+++ First/NewFile.py
@@ -1,1 +1,13 @@
-# 
+# Указав номер четверти прямоугольной системы координат, показать допустимые значения координат для точек этой четверти
+
+def answer(number):
+    if number == 1:
+        return 'x >= 0, y >= 0'
+    elif number == 2:
+        return "x <= 0, y >= 0"
+    elif number == 3:
+        return "x <= 0, y <= 0"
+    elif number == 4:
+        return "x >= 0, y <= 0"
+
+print(answer(3))
