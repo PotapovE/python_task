cc18e82c9cfc0d1b816fa0f89d4eaadbaf2a79b8
PotapovE <potapovomsk@gmail.com>
2022-02-13 19:54:12 +0300

    Less 3, Task 2 - Функция сложения

--- First/Python_Lessons_3.py
+++ First/Python_Lessons_3.py
@@ -3,3 +3,13 @@ def hello ():
     print("Привет, Мир!")
 
 hello()
+
+# Функция сложения
+x = int(input("Введите число - "))
+y = int(input("Введите число - "))
+
+def sum (a, b):
+    return a + b
+
+z = sum (x, y)
+print (z)
