a01171e4ef8c36fabac2860d534542c2d181f081
PotapovE <potapovomsk@gmail.com>
2022-02-14 22:06:48 +0300

    Less 6, Task 2 - Словарь и методы работы с ним

--- First/Python_Lessons_6.py
+++ First/Python_Lessons_6.py
@@ -3,3 +3,18 @@ tuple = ("chek", 3, 5,)
 # tuple[1] = 7 # Eror - кортеж неизменяемый тип данных
 print(tuple)
 print(list(tuple)) # Из кортежа в список
+
+# Словарь
+dict = {"apple": "red", "banana": "yelow", "limon": "yelow"}
+print(dict)
+for i in dict.keys(): # .keys - Выводит ключи словаря
+    print(i)
+for i in dict.values(): # .value - Выводит знвчения словаря
+    print(i)
+for i in dict.items(): # .items - Выводит словарЬ
+    print(i)
+print(dict["apple"]) # Вывод значения по ключу
+dict["apple"] = "green" # Замена значения по ключу
+print(dict["apple"])
+del(dict["banana"]) # del() - Удаление элемента из словаря
+print(dict)
