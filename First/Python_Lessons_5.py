04215978a2521dd2799bc1c209b7954abfea229c
PotapovE <potapovomsk@gmail.com>
2022-02-14 21:16:51 +0300

    Less 5, Task 2 - Вывод элементов списка

--- First/Python_Lessons_5.py
+++ First/Python_Lessons_5.py
@@ -6,3 +6,11 @@ feat = [3, 5.6, 'Hi']
 print(feat)
 multi = [4, 6.7, 'chek', feat] 
 print(multi)
+
+# Вывод элементов списка
+names = ["Dog", "Cat", "Caw"]
+print(names[1]) # Индекс
+print(names[-1]) # С конца списка
+
+for name in names:
+    print(name)
