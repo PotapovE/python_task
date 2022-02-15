6d79dcbf686fa8be4b7a54792d6ee1229bb1d0ee
PotapovE <potapovomsk@gmail.com>
2022-02-15 22:30:01 +0300

    Less 8, Task 1 - работа с модулями в проекте

---
+++ First/Python_Lessons_8.py
@@ -0,0 +1,6 @@
+# Модули
+# import math # - Полный импорт модуля
+# import math as n # - Полный импорт модуля с изменением имени вызова модуля
+from math import factorial # - Импорт только одной функции в модуле
+
+print(factorial(10))
