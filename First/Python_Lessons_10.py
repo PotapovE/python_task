56ab95f8af1abb5eb52d1b47e141ddea312c17b0
PotapovE <potapovomsk@gmail.com>
2022-02-15 23:32:02 +0300

    Less 10, Task 1 - классы в Python

---
+++ First/Python_Lessons_10.py
@@ -0,0 +1,25 @@
+# Классы
+class Gun():
+    """Описание пушки"""
+    def __init__(self, number_gun, kalibr):
+        """Свойства пушки"""
+        self.number_gun = number_gun
+        self.kalibr = kalibr
+        self.age = 0
+    
+    def made(self):
+        """Создает пушку"""
+        print("Пушка " + str(self.number_gun) + " калибром " + str(self.kalibr) + " готова!")
+    def age_of_gun(self, year):
+        """Возраст пушки"""
+        self.age += year
+
+gun1 = Gun(1, 20)
+gun2 = Gun(2, 15)
+
+print(gun2.kalibr) # - обращение к свойствам класса
+gun1.made() # - обращение к методам класса
+print(gun1.age)
+gun2.age_of_gun(5)
+print(gun2.age) 
+print(gun1.age)
