4bbb0baabcb2ee09b146998a1d820ed51dc86b83
PotapovE <potapovomsk@gmail.com>
2022-02-15 21:55:49 +0300

    Less 7, Task 1 - Робота со строками

---
+++ First/Python_Lessons_7.py
@@ -0,0 +1,16 @@
+# Работа со строками
+print("""All 
+day,
+all
+night""")
+print("Hello\t'Hello'\n\"Hello\"")
+text = "hello"
+text1 = "World!"
+print(text + " " + text1)
+print(text * 5)
+print(text[0])
+print(text[0:2])
+print(text[-1])
+print(text.upper()) # .upper() - Заглавные буквы
+print(text.lower()) # .lower() - Прописные буквы
+print(text.capitalize()) # .capitalize() - Делает первую букву заглавной
