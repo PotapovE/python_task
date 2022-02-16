3d01c6c662de1a48735e646f162932812ef4c955
PotapovE <potapovomsk@gmail.com>
2022-02-16 22:20:40 +0300

    Less 11, Task 2 - сортировка списка множеством

--- First/Python_Lessons_11.py
+++ First/Python_Lessons_11.py
@@ -3,3 +3,6 @@ numbers = {1, 5, 7, 11}
 empty_set = set() # set() - Создать пустое множество
 print(numbers)
 print(type(empty_set))
+full_numbers = [1, 1, 5, 7, 90, 7, 81, 3, 11]
+sort_set = set(full_numbers) # Сортировка списка множеством
+print(sort_set)
