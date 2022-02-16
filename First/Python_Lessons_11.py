327eba7be95807332059d8ec12b0a050795491d7
PotapovE <potapovomsk@gmail.com>
2022-02-16 22:47:54 +0300

    Less 11, Task 8 - пересечение множеств .intersection(), &

--- First/Python_Lessons_11.py
+++ First/Python_Lessons_11.py
@@ -1,4 +1,7 @@
 # Множества
+import re
+
+
 numbers = {1, 5, 7, 11, 48, 54, 68}
 empty_set = set() # set() - Создать пустое множество
 print(numbers)
@@ -18,4 +21,8 @@ sort_set.pop() # .pop() - Удаляет первый элемент
 # sort_set.clear() # .clear() - Удаляет все элементы
 print(sort_set) 
 res_set = numbers.union(sort_set) # .union() - Объединение множеств
+res_set = numbers | sort_set # | - Вариант объединения множеств
+print(res_set)
+res_set = numbers.intersection(sort_set) # .intersection() - Пересечение множеств
+res_set = numbers & sort_set # & - Вариант записи пересечения множеств
 print(res_set)
