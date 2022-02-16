ef376d31226d134efdd48e9815fcb03caadc8555
PotapovE <potapovomsk@gmail.com>
2022-02-16 22:24:02 +0300

    Less 11, Task 3 - вывод элементов множества через цикл for

--- First/Python_Lessons_11.py
+++ First/Python_Lessons_11.py
@@ -6,3 +6,7 @@ print(type(empty_set))
 full_numbers = [1, 1, 5, 7, 90, 7, 81, 3, 11]
 sort_set = set(full_numbers) # Сортировка списка множеством
 print(sort_set)
+
+# Меотды работы с множеством
+for i in sort_set: # Вывод элементов множества
+    print(i)
