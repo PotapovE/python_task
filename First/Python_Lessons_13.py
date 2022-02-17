396ca7a5190e59b6a1926073b4bd119dfe93015d
PotapovE <potapovomsk@gmail.com>
2022-02-17 13:29:49 +0300

    Less 13, Task 2 - вывод цифр по шаблону \b

--- First/Python_Lessons_13.py
+++ First/Python_Lessons_13.py
@@ -4,3 +4,5 @@ import re
 text = "485734590-543   54%+nugrkjehg4tg3rerg4+g4  r4etet4534erg"
 result = re.search(r"n..r", text) # r"i...j" - Шаблон для поиска с известными первым и последним символом
 print(result)
+result = re.search(r"\d\d\d\d\d\d\d\d", text) # \d - Вывод цифр по шаблону
+print(result)
