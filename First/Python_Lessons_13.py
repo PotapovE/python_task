a169567adb20d8d416e3480ea93cb7eb2a5f1344
PotapovE <potapovomsk@gmail.com>
2022-02-17 13:37:32 +0300

    Less 13, Task 5 - вывод буквы, цифры, _  по шаблону \w (\W - кроме)

--- First/Python_Lessons_13.py
+++ First/Python_Lessons_13.py
@@ -6,7 +6,9 @@ result = re.search(r"n..r", text) # r"i...j" - Шаблон для поиска 
 print(result)
 result = re.search(r"\d\d\d\d\d\d\d\d", text) # \d - Вывод цифр по шаблону
 print(result)
-result = re.search(r"\D", text) # \D - Вывод символа кроме цифры шаблону
+result = re.search(r"\D", text) # \D - Вывод символа кроме цифры по шаблону
 print(result)
 result = re.search(r"\s", text) # \s - Вывод (\S кроме) пробела по шаблону
+print(result)
+result = re.search(r"\w", text) # \w - Вывод (\W кроме) буквы, цифры, "_" по шаблону
 print(result)
