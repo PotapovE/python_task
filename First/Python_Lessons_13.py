9788c09985657da8d74f9aaa4c7260428dd5906f
PotapovE <potapovomsk@gmail.com>
2022-02-17 13:48:57 +0300

    Less 13, Task 7 - диапазон поиска  по шаблону [] ([^] - кроме)

--- First/Python_Lessons_13.py
+++ First/Python_Lessons_13.py
@@ -4,7 +4,7 @@ import re
 text = "485734590-543   54%+nugrkjehg4tg3rerg4+g4  r4etet4534erg"
 result = re.search(r"n..r", text) # r"i...j" - Шаблон для поиска с известными первым и последним символом
 print(result)
-result = re.search(r"\d\d\d\d\d\d\d\d", text) # \d - Вывод цифр по шаблону
+result = re.search(r"\d\d\d\d\d\d\d\d", text) # \d - Вывод цифр по шаблону (\+, \* - продолжает шаблон до другого символа)
 print(result)
 result = re.search(r"\D", text) # \D - Вывод символа кроме цифры по шаблону
 print(result)
@@ -13,4 +13,6 @@ print(result)
 result = re.search(r"\w", text) # \w - Вывод (\W кроме) буквы, цифры, "_" по шаблону
 print(result)
 result = re.search(r"\b73", text) # \b - Вывод (\B середина) начала или конца слова по шаблону
+print(result)
+result = re.search(r"[^3-7]", text) # [] - Диапазон поиска (^) - кроме
 print(result)
