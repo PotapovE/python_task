0dd842d0923497d3c8e2566de02d5da1c1a826f3
PotapovE <potapovomsk@gmail.com>
2022-02-18 22:25:27 +0300

    Less 14, Task 4.1 - lambda функции

--- First/Python_Lessons_14.py
+++ First/Python_Lessons_14.py
@@ -2,7 +2,7 @@
 def area (a, b):
     return a * b
 print(area(5,4))
-print((lambda a, b: a * b) (3, 4))
+print((lambda a, b: a * b) (3, 4)) # Пример функции lambda
 # Вывести большее число
 print((lambda x, y: x if x > y else y) (23, 15))
 # Написать lambda-функцию, принимающую 1 аргумент — сторону квадрата, и возвращающую периметр квадрата.
