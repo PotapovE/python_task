bf04f6461396d6a03ed1a77fc865ed85572dc6b8
PotapovE <potapovomsk@gmail.com>
2022-02-18 23:34:23 +0300

    Less 15, Task 3 - reduse функция

--- First/Python_Lessons_15.py
+++ First/Python_Lessons_15.py
@@ -9,7 +9,6 @@ def func (a, b):
     return a * b
 a = map(func, [3, 6, 7], [3, 4, 0])
 print(list(a))
-
 # Пример с лямбдой
 a = map(lambda a, b: a * b, [3, 9], [5, 8])
 print(list(a))
@@ -19,3 +18,8 @@ def func (a):
     return a % 2 != 0
 a = filter(func, [3, 6, 7])
 print(list(a))
+
+# Функция reduce
+from functools import reduce
+
+print(reduce(lambda x, y: x * y, [1, 3, 4, 5]))
