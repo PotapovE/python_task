c449db5afd495a8aa63b9782f98641864424bb03
PotapovE <potapovomsk@gmail.com>
2022-02-18 23:42:54 +0300

    Less 15, Task 4.1 - map, filter, reduce, zip функции

--- First/Python_Lessons_15.py
+++ First/Python_Lessons_15.py
@@ -10,22 +10,22 @@ def func (a, b):
 a = map(func, [3, 6, 7], [3, 4, 0])
 print(list(a))
 # Пример с лямбдой
-a = map(lambda a, b: a * b, [3, 9], [5, 8])
+a = map(lambda a, b: a * b, [3, 9], [5, 8]) # map() - применяет функции к последовательности
 print(list(a))
 
 # Функция filter
 def func (a):
     return a % 2 != 0
-a = filter(func, [3, 6, 7])
+a = filter(func, [3, 6, 7]) # filter() - Фильтрует последовательность по задонному условию
 print(list(a))
 
 # Функция reduce
 from functools import reduce
 
-print(reduce(lambda x, y: x * y, [1, 3, 4, 5]))
+print(reduce(lambda x, y: x * y, [1, 3, 4, 5])) # reduse() - Перемножает (сладывает и т.д) все числа в последовательности
 
 # Функция zip
 a = [1, 6, 8, 3, 7]
 b = 'dsfgfghgfgs'
-res = zip(a, b)
+res = zip(a, b) # zip() - Складывает значения последовательностей в кортежи
 print(list(res))
