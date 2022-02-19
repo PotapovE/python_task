e7760a17d59e91a622754d6e32f2c85070aeecc8
PotapovE <potapovomsk@gmail.com>
2022-02-19 20:33:28 +0300

    Less 18, Task 2 - Вложенные функции, замыкания

--- First/Python_Lessons_18.py
+++ First/Python_Lessons_18.py
@@ -4,3 +4,13 @@ def message (name):
         return "Hello, " + name + "!"
     return sent_message()
 print(message("Маша"))
+
+# Замыкания
+def key (x):
+    def open_key (y):
+        return x + y
+    return open_key
+save_key = key(5)
+print(save_key)
+print(save_key(6))
+print(save_key(11))
