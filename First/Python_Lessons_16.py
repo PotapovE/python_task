fcfd87a796bf7000f6a6fbcc9fd79e2d75b85171
PotapovE <potapovomsk@gmail.com>
2022-02-19 19:33:13 +0300

    Less 16, Task 2 - Аргументы *args и **kwargs

--- First/Python_Lessons_16.py
+++ First/Python_Lessons_16.py
@@ -1,4 +1,10 @@
-# Аргументы
+# Аргументы *args
 def func (*numbers):
     print(sum(numbers))
 func(5, 6, 1, 8)
+
+# Аргументы **kwargs
+def kek (**klop):
+    for x, y in klop.items():
+        print (x, ":", y)
+kek(a = 15, b = 51, k = 66)
