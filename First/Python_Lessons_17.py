8dc059d38cb219787cf1a21b01c48b9e1cfc80f2
PotapovE <potapovomsk@gmail.com>
2022-02-19 19:57:52 +0300

    Less 17, Task 2 - Генератор списка с полным ветвлением

--- First/Python_Lessons_17.py
+++ First/Python_Lessons_17.py
@@ -1,3 +1,6 @@
 # Генератор списка
 s = [i for i in range(21) if i % 5 == 0]
 print(s)
+# Пример с полным ветвлением
+s = [i ** 2 if i > 0 else i ** 3 for i in range(-10, 11) if i % 2 != 0]
+print(s)
