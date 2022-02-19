128a8a6e89f3809d7c8a71c33a540ec8b2671086
PotapovE <potapovomsk@gmail.com>
2022-02-19 20:00:00 +0300

    Less 17, Task 3 - Генератор множества

--- First/Python_Lessons_17.py
+++ First/Python_Lessons_17.py
@@ -1,6 +1,13 @@
 # Генератор списка
+from re import S
+
+
 s = [i for i in range(21) if i % 5 == 0]
 print(s)
 # Пример с полным ветвлением
 s = [i ** 2 if i > 0 else i ** 3 for i in range(-10, 11) if i % 2 != 0]
+print(s)
+# Генератор множества
+l = [45, -45, 45, 55, -55, -55]
+s = {i for i in l}
 print(s)
