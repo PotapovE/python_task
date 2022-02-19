d2535c7493fb64c29f4b657900ba2a6525be5860
PotapovE <potapovomsk@gmail.com>
2022-02-19 20:02:26 +0300

    Less 17, Task 4 - Генератор списка, множества, словаря

--- First/Python_Lessons_17.py
+++ First/Python_Lessons_17.py
@@ -1,7 +1,4 @@
 # Генератор списка
-from re import S
-
-
 s = [i for i in range(21) if i % 5 == 0]
 print(s)
 # Пример с полным ветвлением
@@ -11,3 +8,6 @@ print(s)
 l = [45, -45, 45, 55, -55, -55]
 s = {i for i in l}
 print(s)
+# Генератор словаря
+d = {i: i ** 2 for i in l if i > 0}
+print(d)
