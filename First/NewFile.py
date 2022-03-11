2134bc19314ac1ae8f9fa296ca173c25e60baee4
PotapovE <potapovomsk@gmail.com>
2022-03-11 22:28:53 +0300

    Решение задач на GB 14

--- First/NewFile.py
+++ First/NewFile.py
@@ -1,8 +1,2 @@
-# 13. Пользователь задаёт две строки. Определить количество вхождений одной строки в другой.
-text_user, user_text = input(), input()
-user_text_list = user_text.split()
-counter = 0
-for i in user_text_list:
-    if i == text_user:
-        counter += 1
-print(counter)
+# 14. Подсчитать сумму цифр в вещественном числе.
+print(sum([int(i) for i in str(abs(float(input('Введите число через точку: ')))).replace('.','')]))
